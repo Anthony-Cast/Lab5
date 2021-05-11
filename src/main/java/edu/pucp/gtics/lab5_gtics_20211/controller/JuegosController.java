219ec2943284746b195a808301a5b4758a97b6b7
LiziAC05 <lizbeth_aqcast@hotmail.com>
2021-05-10 20:04:13 -0500

    Modificaciones en Juegos, ya está el DTO que corresponde.

--- src/main/java/edu/pucp/gtics/lab5_gtics_20211/controller/JuegosController.java
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20211/controller/JuegosController.java
@@ -27,10 +27,32 @@ public class JuegosController {
 
     @Autowired
     JuegosRepository juegosRepository;
+    @GetMapping("/lista")
+    public String listaJuegos (Model model){
+               model.addAttribute("");
+               return "juegos/comprado";
+    }
 
     @GetMapping(value = {"", "/", "/vista"})
-    public String vistaJuegos (Model model ){
-        return "juegos/vista";
+    public String vistaJuegos ( ... ){
+               /** Completar */
+    }
+
+    @GetMapping( ... )
+    public String nuevoJuegos(Model model, @ModelAttribute("juego") Juegos juego){
+               /** Completar */
+    }
+
+    @GetMapping( ... )
+    public String editarJuegos(@RequestParam("id") int id, Model model){
+                /** Completar */
+
+    }
+
+    @PostMapping( ... )
+    public String guardarJuegos(Model model, RedirectAttributes attr, @ModelAttribute("juego") @Valid Juegos juego, BindingResult bindingResult ){
+                /** Completar */
+
     }
 
     @GetMapping("/juegos/borrar")
